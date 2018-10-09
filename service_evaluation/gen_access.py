ec778102c117f98cc1c88ac4d8c644510887356d
taratep <taratep.sira@gmail.com>
2018-10-09 14:54:39 +0700

    gen test access

---
+++ service_evaluation/gen_access.py
@@ -0,0 +1,40 @@
+
+from random import choice, randint
+from datetime import timedelta
+from django.utils import timezone as tz
+from django.db import transaction
+from app.models import AccessCount
+
+__PACKS = [
+    {
+        'url': 'http://analytics.service.sci.tu.ac.th/eval/',
+        'path': '/eval/peoplefinder',
+        'host': 'analytics.service.sci.tu.ac.th',
+    },
+    {
+        'url': 'http://analytics.service.sci.tu.ac.th/eval/research',
+        'path': '/eval/research',
+        'host': 'analytics.service.sci.tu.ac.th',
+    },
+    {
+        'url': 'http://peoplefinder.service.sci.tu.ac.th/',
+        'path': '/',
+        'host': 'peoplefinder.service.sci.tu.ac.th',
+    },
+]
+
+protocol = 'http:'
+ip = '127.0.0.1'
+os = ['Window', 'Linux', 'Android', 'Mac OS']
+
+with transaction.atomic():
+    for i in range(10):
+        pack = {
+            **choice(__PACKS),
+            'protocol': protocol,
+            'ip': ip,
+            'os': choice(os),
+        }
+        inst = AccessCount.objects.create(**pack)
+        inst.timestamp = tz.now() + timedelta(hours=randint(-(24*30*1), 0))
+        inst.save()
